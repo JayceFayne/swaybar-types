7274a05a0e6444846869c94b2fc49e6562536823
Rex Hackbro <rex.hackbro@mailbox.org>
2020-09-11 15:23:34 +0200

    fix `Button` variants

--- src/click.rs
+++ src/click.rs
@@ -1,5 +1,4 @@
 use serde::{Deserialize, Serialize};
-use std::convert::TryFrom;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Click {
@@ -16,31 +15,41 @@ pub struct Click {
 }
 
 #[repr(u32)]
+#[non_exhaustive]
 #[derive(Debug, Copy, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(try_from = "u32")]
+#[serde(from = "u32")]
 #[serde(into = "u32")]
 pub enum Button {
-    None = 0,
-    Left = 1,
-    Middle = 2,
-    Right = 3,
-    WheelUp = 4,
-    WheelDown = 5,
-    WheelLeft = 6,
-    WheelRight = 7,
+    None,
+    Left,
+    Middle,
+    Right,
+    WheelUp,
+    WheelDown,
+    WheelLeft,
+    WheelRight,
+    Other(u32),
 }
 
 impl From<Button> for u32 {
     fn from(button: Button) -> Self {
-        button as u32
+        match button {
+            Button::None => 0,
+            Button::Left => 1,
+            Button::Middle => 2,
+            Button::Right => 3,
+            Button::WheelUp => 4,
+            Button::WheelDown => 5,
+            Button::WheelLeft => 6,
+            Button::WheelRight => 7,
+            Button::Other(u) => u,
+        }
     }
 }
 
-impl TryFrom<u32> for Button {
-    type Error = String;
-
-    fn try_from(value: u32) -> Result<Self, Self::Error> {
-        Ok(match value {
+impl From<u32> for Button {
+    fn from(value: u32) -> Self {
+        match value {
             0 => Button::None,
             1 => Button::Left,
             2 => Button::Middle,
@@ -49,7 +58,7 @@ impl TryFrom<u32> for Button {
             5 => Button::WheelDown,
             6 => Button::WheelLeft,
             7 => Button::WheelRight,
-            _ => return Err(format!("click '{}' not implemented", value)),
-        })
+            v => Button::Other(v),
+        }
     }
 }
