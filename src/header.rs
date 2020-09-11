ee32114ba1de8f4059a0d08bd89d0dc68be030d4
Rex Hackbro <rex.hackbro@mailbox.org>
2020-09-11 15:24:06 +0200

    add `Version` enum

--- src/header.rs
+++ src/header.rs
@@ -1,12 +1,39 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
+use std::convert::TryFrom;
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Header {
-    pub version: u8, //currently this must be 1
+    pub version: Version,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub stop_signal: Option<u32>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub cont_signal: Option<u32>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub click_events: Option<bool>,
+}
+
+#[repr(u8)]
+#[non_exhaustive]
+#[derive(Debug, Copy, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(try_from = "u8")]
+#[serde(into = "u8")]
+pub enum Version {
+    One = 1,
+}
+
+impl From<Version> for u8 {
+    fn from(version: Version) -> Self {
+        version as u8
+    }
+}
+
+impl TryFrom<u8> for Version {
+    type Error = String;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        Ok(match value {
+            1 => Version::One,
+            _ => return Err(format!("invalid version number '{}'", value)),
+        })
+    }
 }
